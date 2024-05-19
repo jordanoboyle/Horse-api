9e61ab303b2c855543cd61405937576e9999fbce
jordanoboyle <jmcmcode@gmail.com>
2024-05-19 02:19:11 -0600

    tax, price total, kilo weight

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,2 +1,19 @@
 class Horse < ApplicationRecord
+
+  def tax_total
+    tax_rate = 0.095
+    taxes = price.to_f * tax_rate
+    return taxes
+  end
+
+  def price_total 
+    total = price + tax_total
+    return total
+  end
+
+  def kilos
+    weight_kilos = weight * 0.455
+    return weight_kilos.round(2)
+  end
+
 end
--- app/views/horses/_horse.json.jbuilder
+++ app/views/horses/_horse.json.jbuilder
@@ -5,6 +5,9 @@ json.id horse.id
 json.name horse.name
 json.color horse.color
 json.weight horse.weight
+json.kilos horse.kilos
 json.price horse.price
+json.tax_total horse.tax_total
+json.price_total horse.price_total
 json.created_at horse.created_at
 json.updated_at horse.updated_at
